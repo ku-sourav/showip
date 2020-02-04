c9c822088a6bbebe7c34c0b417fd75c9478dfb8e
kumar sourav <sourav.jb1988@gmail.com>
2020-02-04 12:10:43 +0530

    showip: Add comments
    Signed-off-by: kumar sourav <sourav.jb1988@gmail.com>

--- showip.c
+++ showip.c
@@ -12,29 +12,35 @@ int main(int argc, char**argv) {
 	int err = 0;
 
 	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_family = AF_UNSPEC;		//Allow IPv4 or IPv6
+	hints.ai_socktype = SOCK_STREAM;	//TCP socket
 
+	//argv[1] is the website name example "www.google.com"
 	if ((err = getaddrinfo(argv[1], NULL, &hints, &res)) != 0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(err));
 		exit(EXIT_FAILURE); 	
 	}
 	
+	//res contains the linked list of all the addresses which we iterate
 	for (ptr = res; ptr != NULL; ptr = ptr->ai_next) {
-		char IP[INET6_ADDRSTRLEN];
+		//INET6_ADDRSTRLEN has enough space for IPv4 and IPv6
+		char IP[INET6_ADDRSTRLEN];					
 		void *addr;
 		char *iptype;
-		if (ptr->ai_family == AF_INET) {
+
+		if (ptr->ai_family == AF_INET) {		//IPv4
 			struct sockaddr_in *saddr;
 			saddr = (struct sockaddr_in *)ptr->ai_addr;
 			addr = &(saddr->sin_addr);
 			iptype = "IP4";	
-		} else {
+		} else {								//IPv6
 			struct sockaddr_in6 *saddr6;
 			saddr6 = (struct sockaddr_in6 *)ptr->ai_addr;
 			addr = &(saddr6->sin6_addr);
 			iptype = "IP6";
 		}
+
+		//convert ip addr to presentable format 
 		if (inet_ntop(ptr->ai_family, addr, IP, INET6_ADDRSTRLEN) == NULL) {
 			perror("inet_ntop");
 			exit(EXIT_FAILURE);
